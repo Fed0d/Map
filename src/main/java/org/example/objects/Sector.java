58e8081fcff80f258caaec41d707e3a309388c8e
Fed0d <loldffgf@yandex.ru>
2024-03-01 03:24:53 +0300

    Размапленный Sector

--- src/main/java/org/example/objects/Sector.java
+++ src/main/java/org/example/objects/Sector.java
@@ -1,11 +1,28 @@
 package org.example.objects;
 
-import com.bbn.openmap.omGraphics.OMPoly;
+import com.bbn.openmap.omGraphics.OMArc;
 
-public class Sector {
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sectors")
+public class Sector extends OMArc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "latitudeCenter")
     private double latitudeCenter;
+    @Column(name = "longitudeCenter")
     private double longitudeCenter;
+    @Column(name = "xRadius")
     private double xRadius;
+    @Column(name = "yRadius")
     private double yRadius;
+    @Column(name = "start")
+    private double start;
+    @Column(name = "extent")
+    private double extent;
 }
